ba5701682c2670d695581794e43cfb7bb6adba85
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2018-07-16 23:32:02 +0900

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+	// Set the number of years
+	let year_text = document.getElementById('year').innerText, date = new Date();
+	document.getElementById('year').innerText = year_text.replace('YY', `${my_year(date.getFullYear(), date.getMonth(), date.getDate())}`);
+
+	document.getElementById('back-btn').addEventListener('click', () => { scroll_page(0) });
+	Array.from(document.getElementsByTagName('button')).map((dom, index) => {
+		dom.addEventListener('click', () => {
+			scroll_page(index);
+		});
+	});
+};
+
+function my_year(year, month, date) {
+	return year - 2002 - (month < 3 || (month > 2 && date < 5));
+}
+
+function scroll_page(id) {
+	let scroll_to = [0, 1640, 2095, 3550][id],
+		now_scroll = window.pageYOffset,
+		speed = 13;
+	
+	let loop = setInterval(() => {
+		now_scroll += (scroll_to - now_scroll) / speed;
+		window.scrollTo(0, now_scroll);
+		
+		if (Math.abs(now_scroll - scroll_to) < 0.1) {
+			clearInterval(loop);
+		}
+	}, 10);
+}
