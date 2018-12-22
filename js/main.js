53404a5c6f9e21b473f38a6d83fcc2226aeffcdc
JPNYKW <ilikegamesme@gmail.com>
2018-12-23 01:33:42 +0900

    update v1.1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 (_ => {
-      const init = _ => {
+      onload = _ => {
             console.log('OK');
             const goTopButton = document.getElementById('go-top');
             goTopButton.onclick = _ => window.scrollTo(0, 0);
-      };
 
-      window.onload = init;
+            let tag = location.href.split('?');
+            tag.shift();
+
+            tag.join('?').split('&').map(data => {
+                  let expand = data.split('=');
+                  let name = expand[0];
+                  expand.shift();
+
+                  let value = expand.join('=');
+                  switch (name.toLowerCase()) {
+                        case 'ar':
+                              if (value === 'true') {
+                                    // console.log('AR-Mode');
+                                    document.getElementsByClassName('ar-mode')[0].classList.remove('hide');
+                              }
+                              break;
+                  }
+            });
+      };
 })();
