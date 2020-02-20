be1061b75d310e03671d256fb402631682f09e9f
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-02-21 03:31:25 +0900

    :recycle: Components design :construction: Display multiple cards :sparkles: Added chips

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-
-/* components */
 import {
   Avatar,
   Card,
@@ -9,23 +7,31 @@ import {
   CardMedia,
   CardContent,
   CardActionArea,
+  Chip,
   Typography
 } from '@material-ui/core';
-
-/* icons */
 import {
   TwitterIcon
 } from '@material-ui/icons';
 
 const StyledCard = styled(Card)`
   max-width: 350;
+  margin: 10;
+`;
+
+const StyledCardMedia = styled(CardMedia)`
+  max-height: 200;
+`;
+
+const StyledChip = styled(Chip)`
+  margin: 5 2;
 `;
 
 const About = (props) => {
   return (
     <StyledCard>
       <CardActionArea>
-        <CardMedia
+        <StyledCardMedia
           component="img"
           image={props.thumbnail}
         />
@@ -36,6 +42,18 @@ const About = (props) => {
           </Typography>
 
           <Typography variant="body2" color="textSecondary" component="p">
+            {props.date}
+          </Typography>
+
+          {
+            props.languages.map(lang => {
+              return (
+                <StyledChip key={lang} label={lang} color="primary" size="small" />
+              );
+            })
+          }
+
+          <Typography variant="body1" color="textSecondary" component="p">
             {props.description}
           </Typography>
         </CardContent>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-/* components */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Project from './components/Project';
-/* resources */
 import Icon from './assets/images/icon.png';
 
 const App = () => {
   return (
-    <Project
-      thumbnail={Icon}
-      title="haruto hirakawa"
-      description="I was born in March 5th 2002"
-    />
+    <div>
+      <Project
+        thumbnail={Icon}
+        title='haruto hirakawa'
+        date='March 5th 2002'
+        languages={['C', 'Rust', 'Assembly']}
+        description='I love cats and not love humans'
+      ></Project>
+
+      <Project
+        thumbnail={Icon}
+        title='haruto hirakawa'
+        date='March 5th 2002'
+        languages={['C', 'Rust', 'Assembly']}
+        description='I love cats and not love humans'
+      ></Project>
+    </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
 
