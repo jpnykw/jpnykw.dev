79524cfb287532216bcba06533964bd24706209b
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-09-09 11:16:19 +0900

    :recycle: Fix the method of calculate position (About)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,130 +15,148 @@ import Icon from '../assets/images/icon.png'
 
 const AboutMe: React.FC<{}> = (props) => {
   return (
-    <Contents className="center">
-      {/* MOTION */}
-      <Elements
-        className="loop center"
+    <>
+      <Motion>
+        {/* MOTION */}
+        <Elements
+          className="loop center"
 
-        ref={ref => {
-          anime({
-            targets: '.loop .el',
-            translateX: function(el: any) {
-              return Number(el.getAttribute('data-x')) + anime.random(-20, 30)
-            },
-            translateY: function(el: any) {
-              return Number(el.getAttribute('data-y')) + anime.random(-50, 50)
-            },
-            scale: 2,
-            rotate: function() { return anime.random(-360, 360) },
-            borderRadius: function() { return ['50%', anime.random(10, 35) + '%'] },
-            duration: function() { return anime.random(1400, 1800) },
-            delay: function() { return anime.random(900, 1100) },
-            opacity: function(el: any) {
-              return Number(el.getAttribute('data-opacity')) || 1
-            },
-            direction: 'alternate',
-            loop: true
-          })
-        }}
+          ref={ref => {
+            anime({
+              targets: '.loop .el',
+              translateX: function(el: any) {
+                return Number(el.getAttribute('data-x')) + anime.random(-20, 30)
+              },
+              translateY: function(el: any) {
+                return Number(el.getAttribute('data-y')) + anime.random(-50, 50)
+              },
+              scale: 2,
+              rotate: function() { return anime.random(-360, 360) },
+              borderRadius: function() { return ['50%', anime.random(10, 35) + '%'] },
+              duration: function() { return anime.random(1400, 1800) },
+              delay: function() { return anime.random(900, 1100) },
+              opacity: function(el: any) {
+                return Number(el.getAttribute('data-opacity')) || 1
+              },
+              direction: 'alternate',
+              loop: true
+            })
+          }}
 
-        style={{
-          height: '100px',
-          width: '100px',
-        }}
-      >
-        <Element data-x="-200" data-y="-60" className="small circle el" style={{opacity: '0', width: '14px', height: '14px', background: 'rgba(164, 255, 79)'}} />
-        <Element data-x="200" data-y="190" className="small circle el" style={{opacity: '0', width: '12px', height: '12px', background: 'rgba(79, 255, 164)'}} />
-        <Element data-x="170" data-y="-260" className="small circle el" style={{opacity: '0', width: '10px', height: '10px', background: 'rgba(255, 164, 79)'}} />
-      </Elements>
+          style={{
+            height: '100px',
+            width: '100px',
+          }}
+        >
+          <Element data-x="-200" data-y="-60" className="small circle el" style={{opacity: '0', width: '14px', height: '14px', background: 'rgba(164, 255, 79)'}} />
+          <Element data-x="200" data-y="190" className="small circle el" style={{opacity: '0', width: '12px', height: '12px', background: 'rgba(79, 255, 164)'}} />
+          <Element data-x="170" data-y="-260" className="small circle el" style={{opacity: '0', width: '10px', height: '10px', background: 'rgba(255, 164, 79)'}} />
+        </Elements>
 
-      <Elements
-        className="star center"
+        <Elements
+          className="star center"
 
-        ref={ref => {
-          anime({
-            targets: '.star .el',
-            translateX: function(el: any) {
-              return Number(el.getAttribute('data-x')) + anime.random(-20, 30)
-            },
-            translateY: function(el: any) {
-              return Number(el.getAttribute('data-y')) + anime.random(-50, 50)
-            },
-            duration: function() {
-              return anime.random(1800, 2600)
-            },
-            delay: function() {
-              return anime.random(1200, 1300)
-            },
-            borderRadius: 100,
-            opacity: 0.2,
-            scale: 3,
-          })
-        }}
-      >
-        {
-          Array(14).fill(null).map((_, id) => {
-            const x = (200 + Math.random() * 650) * (Math.random() < .5 ? 1 : -1)
-            const y = (150 + Math.random() * 350) * (Math.random() < .5 ? 1 : -1)
+          ref={ref => {
+            anime({
+              targets: '.star .el',
+              translateX: function(el: any) {
+                return Number(el.getAttribute('data-x')) + anime.random(-20, 30)
+              },
+              translateY: function(el: any) {
+                return Number(el.getAttribute('data-y')) + anime.random(-50, 50)
+              },
+              duration: function() {
+                return anime.random(1800, 2600)
+              },
+              delay: function() {
+                return anime.random(1200, 1300)
+              },
+              borderRadius: 100,
+              opacity: 0.2,
+              scale: 3,
+            })
+          }}
+        >
+          {
+            Array(14).fill(null).map((_, id) => {
+              const x = (200 + Math.random() * 650) * (Math.random() < .5 ? 1 : -1)
+              const y = (150 + Math.random() * 350) * (Math.random() < .5 ? 1 : -1)
 
-            return (
-              <Element
-                key={id} data-x={x} data-y={y} className="small circle el"
-                style={{opacity: '0', width: '2px', height: '2px', background: 'rgba(244, 244, 244, 10)'}}
-              />
-            )
-          })
-        }
-      </Elements>
+              return (
+                <Element
+                  key={id} data-x={x} data-y={y} className="small circle el"
+                  style={{opacity: '0', width: '2px', height: '2px', background: 'rgba(244, 244, 244, 10)'}}
+                />
+              )
+            })
+          }
+        </Elements>
+      </Motion>
 
-      {/* ICON */}
-      <StyledImg
-        src={Icon}
+      <Contents>
 
-        ref={ref => {
-          anime({
-            targets: ref,
-            opacity: 1,
-            translateY: -50,
-            // easing: 'easeInOutExpo',
-            easing: 'spring(1, 80, 10, 0)',
-            delay: 800,
-            duration: 400,
-          })
-        }}
-      />
+        <div style={{textAlign: 'center'}}>
+          {/* ICON */}
+          <StyledImg
+            src={Icon}
 
-      <Margin />
+            ref={ref => {
+              anime({
+                targets: ref,
+                opacity: 1,
+                translateY: -50,
+                // easing: 'easeInOutExpo',
+                easing: 'spring(1, 80, 10, 0)',
+                delay: 800,
+                duration: 400,
+              })
+            }}
+          />
 
-      {/* NAME */}
-      <MotionText
-        text="Haruto Hirakawa"
-        delay={1100}
-        cooltime={30}
-      />
+          {/* NAME */}
+          <div>
+            <MotionText
+              text="Haruto Hirakawa"
+              delay={1100}
+              cooltime={30}
+            />
+          </div>
 
-      <Container>
-        <Link delay={1280} icon={<TwitterIcon />} text="Twitter" href="https://twitter.com/jpnykw" />
-        <Link delay={1370} icon={<GitHubIcon />} text="GitHub" href="https://github.com/jpnykw" />
-      </Container>
-    </Contents>
+          <div>
+            <Link delay={1280} icon={<TwitterIcon />} text="Twitter" href="https://twitter.com/jpnykw" />
+            <Link delay={1370} icon={<GitHubIcon />} text="GitHub" href="https://github.com/jpnykw" />
+          </div>
+        </div>
+      </Contents>
+    </>
   )
 }
 
 export default AboutMe
 
 /* naming */
-const Contents = styled.div``
 const Elements = styled.div``
 const Element = styled.div``
 
 /* Styling */
+const Motion = styled.div`
+  position: relative;
+  top: 540px;
+`
+
+const Contents = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-items: center;
+`
+
 const StyledImg = styled.img`
-  border-radius: 90px;
   width: 180px;
-  opacity: 0;
   position: relative;
   top: 50px;
+  border-radius: 90px;
+  opacity: 0;
 `
 
 const Margin = styled.div`
