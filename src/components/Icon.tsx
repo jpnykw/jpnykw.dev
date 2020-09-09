cdd4930bca4f3e887e661a96bfc487d6f1071653
JPNYKW <haruto_17n1101083@nnn.ed.jp>
2020-09-09 23:23:29 +0900

    :construction: Add new component Icon for display svg

---
+++ src/components/Icon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface Props {}
+
+const Icon: React.FC<Props> = (props) => {
+  return (
+    <></>
+  )
+}
+
+export default Icon
+
