6ad7328f80edb29e6375185eb72cc6c5c1273cab
jpnykw <jpnykw@gmail.com>
2022-12-08 12:02:56 +0900

    feat: シャッターアニメーションを実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import styled from 'styled-components'
 
 import About from './pages/About'
+import NotFound from './pages/NotFound'
 import Wtf from './pages/Wtf'
 import './assets/css/font.css'
 import './assets/css/index.css'
@@ -19,13 +20,45 @@ const Credit = styled.div`
 	bottom: 0;
 `
 
+const Sh = styled.div`
+	background: black;
+	width: 100vw;
+	height: 50vh;
+	z-index: 1;
+
+	position: absolute;
+	${({ pos, y }: { pos: 'bottom' | 'top', y: number }) => `
+		${pos}: ${-y}px;
+		left: 50%;
+		transform: translateX(-50%);
+	`}
+`
+
+const MaxY = innerHeight / 2
+
 const App = () => {
+	const [y, setY] = useState<number | null>(null)
+
+	const animation = useCallback(() => {
+		setY((y) => y === null ? null : y + (MaxY - y) / 14)
+	}, [])
+
+  // @ts-ignore
+	useEffect(() => {
+		if (y === null) return setTimeout(() => setY(0), 460)
+		if (MaxY - y < 1) return null
+		requestAnimationFrame(animation)
+	}, [y])
+
   return (
     <>
+			<Sh pos={'bottom'} y={y || 0} />
+			<Sh pos={'top'} y={y || 0} />
 			<BrowserRouter>
 				<Routes>
 					<Route index element={<About />} />
 					<Route path='wtf' element={<Wtf />} />
+          <Route path='*' element={<NotFound />} />
 				</Routes>
 			</BrowserRouter>
 			<Credit className="credit">2022 &copy; jpnykw</Credit>
